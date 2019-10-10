476c7786e8e6f03e0d133310fbf755ff2ee62f25
Russell Hyde <russ_hyde@hotmail.com>
2019-10-10 12:05:36 +0100

    given an error string, print out some context
    New exported functions `explain` and `wtf`.
    
    These should be called like `explain("some error message")`
    and will display to the user some info about the likely
    cause of an error message.
    
    At present, the only error description that is available
    is for non-subsettability. An apology function is also
    present which apologises if we can't explain some error
    message.

--- tests/testthat/test-explain.R
+++ tests/testthat/test-explain.R
@@ -1,4 +1,8 @@
+###############################################################################
+
 context("Test that explain() and wtf() can be called")
+
+###############################################################################
 
 test_that("explain() is a synonym for wtf()", {
   my_mock <- mock()
@@ -7,6 +11,7 @@ test_that("explain() is a synonym for wtf()", {
   expect_called(my_mock, 1)
   expect_args(my_mock, 1, "some-error")
 })
+
 
 test_that("input to explain() is either a string or no-args", {
   invalid_input <- list(
@@ -24,23 +29,36 @@ test_that("input to explain() is either a string or no-args", {
   }
 })
 
+
 test_that("explain(x) calls suggest(x)", {
   # suggest(x) should identify the relevant document to show to the user
   # display(x) should show the relevant document to the user
   # - first implementation of `display` should call `help`
 })
 
-test_that("explain('something') calls help(x) where x is a local function", {
-  my_help <- mock()
-  stub(explain, "help", my_help)
-  expect_null(
-    explain("object of type 'closure' is not subsettable")
+
+test_that("explain(x) calls display(some_function)", {
+  my_display <- mock()
+  stub(explain, "display", my_display)
+
+  # When an Error message of the form 'object of type <xxx> is not subsettable'
+  # is passed to explain(), the description for a non-subsettable function
+  # should be displayed
+  expect_display(
+    "... object of type 'closure' is not subsettable",
+    not_subsettable
   )
-  expect_called(my_help, 1)
-  expect_args(my_help, 1, not_subsettable)
+
+  # If an uncommon error message is passed into explain(), apologise for not
+  # having an explanation for that error
+  expect_display(
+    "... some random error message",
+    apology
+  )
 })
 
-#test_that("explain() checks the most recent error message", {
+
+# test_that("explain() checks the most recent error message", {
 #  tryCatch(
 #    mean[1],
 #    error = function(e) {
@@ -48,4 +66,4 @@ test_that("explain('something') calls help(x) where x is a local function", {
 #    }
 #  )
 #  # `e` is of class: "simpleError" "error"       "condition"
-#})
+# })
